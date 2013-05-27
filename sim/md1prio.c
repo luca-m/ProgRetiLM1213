fe538386ead75a2fb4e1eef0c1923221adaeb4ed
Luca M <lucam.ko@gmail.com>
2013-05-27 23:30:59 +0200

    md1prio ?

---
+++ sim/md1prio.c
@@ -0,0 +1,55 @@
+#include "xx1prio.h"
+
+/*
+    This function generates an instance of an exponential random variable
+    Arguments:
+            param = parameter of the exponential distribution, i.e., inverse of the mean value
+*/
+double expon(double param) {
+    // Declare some useful variables
+    int rnd_num;
+    double unif, val;
+    // Generate a uniform random number between 0 and 1
+    rnd_num = rand();
+    if (rnd_num == RAND_MAX) {
+        rnd_num--;
+    }
+    unif = (double) rnd_num / RAND_MAX;
+    // Transform the uniform random variable into an exponential one using the inverse function rule
+    val = -log(1.0 - unif) / param;
+    if (DEBUG) {
+        fprintf(stderr, "DEBUG 05: New exponential random variable with parameter %1.6f: generated value = %1.6f\n", param, val);
+    }
+    return val;
+}
+
+
+/*
+    Service time is exactly what is specified in input of the program (1/mu)
+*/
+double deterministic(double param){
+    double val;
+    val=1.0/param;
+    if (DEBUG) {
+        fprintf(stderr, "DEBUG 05: New deterministic parameter %1.6f: generated value = %1.6f\n", param, val);
+    }
+    return val;
+}
+/*
+    Main program
+*/
+int main(int argc, char *argv[]) {
+    int ret = 0;
+    ret = parse_args(argc, argv);
+    if (ret != 0) {
+        exit(ret);
+    }
+
+    initialize(&expon, &deterministic);
+
+    simulate(&expon, &deterministic);
+
+    prepare_results();
+
+    print_results();
+}
